9b361a05aff528d61224bbe5f7e6ed77520ebe81
Miguelángel Cabrera <mcabrera.dev@gmail.com>
2025-10-19 23:12:31 -0300

    chore: update ESLint configuration for TypeScript files
    - Added ignore pattern for test files in the ESLint configuration.
    - Configured separate ESLint settings for test files, including parser options and specific rules.

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import parser from '@typescript-eslint/parser';
 export default [
   {
     files: ['src/**/*.ts'],
+    ignores: ['src/**/*.test.ts'],
     languageOptions: {
       parser,
       parserOptions: {
@@ -15,6 +16,25 @@ export default [
     },
     rules: {
       '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'warn'
+    }
+  },
+  {
+    files: ['src/**/*.test.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-non-null-assertion': 'warn'
